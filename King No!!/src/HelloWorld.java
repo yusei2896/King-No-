5346f7acd4b80761a5c7e3c87117fbc3317d6504
TOMITA Ryosuke <kd1234702@st.kobedenshi.ac.jp>
2020-11-26 11:57:06 +0900

    てす

--- King No!!/src/HelloWorld.java
+++ King No!!/src/HelloWorld.java
@@ -1,8 +1,18 @@
 
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("テスト");//冨田涼介
+		//テスト(冨田涼介)
+		System.out.println("テスト");
+
+		//ただのテスト
+		System.out.println(hw());
+		System.out.println("ほげほげ");
+	}
+	
+	public static String hw() {
+		return "世界よこんにちは！";
 
 	}
 
