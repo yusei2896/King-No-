776ed821c38c1a595befde6d8c05799081375b9e
TOMITA Ryosuke <kd1234702@st.kobedenshi.ac.jp>
2020-12-20 00:59:16 +0900

    全体的に整理。テキストの追加:1～6年の熟語と解の2つ。難易度を2にしたときに追加したテキストが選択される

--- King No!!/src/TextPazzsample.java
+++ King No!!/src/TextPazzsample.java
@@ -1,5 +1,4 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,7 +8,6 @@ import javax.swing.ImageIcon;
 import java.awt.Font;
 import java.net.URISyntaxException;
 import java.net.URL;
-
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -22,7 +20,6 @@ import java.awt.event.ActionEvent;
 import java.util.*;
 import java.awt.Color;
 
-
 /**
  * @author King no !!
  *
@@ -30,7 +27,6 @@ import java.awt.Color;
 public class TextPazzsample extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	
 	private JTextField textField;
 	private JLabel LeftLabel;
 	private JLabel UpLabel;
@@ -39,11 +35,17 @@ public class TextPazzsample extends JFrame {
 	private JLabel RightLabel;
 	private JLabel HideLabel;
 	
-	int diffculty = 0; //難易度選択0:easy 1:normal 2:hard
-	JLabel[] Labels = {CenterLabel, LeftLabel, UpLabel, DownLabel, RightLabel};
-	String C,L="左",U="上",D="下",R="右";
+	URL allansTxturl = this.getClass().getResource("resources/allansmoji.txt");
+	URL easyTxturl = this.getClass().getResource("resources/one.txt");
+	URL normalTxturl = this.getClass().getResource("resources/one.txt");
+	URL hardTxturl = this.getClass().getResource("resources/alljukugo.txt");
+	URL[] difflist = { easyTxturl, normalTxturl, hardTxturl };
+
+	int diffculty = 2; // 難易度選択0:easy 1:normal 2:hard
+	JLabel[] Labels = { CenterLabel, LeftLabel, UpLabel, DownLabel, RightLabel };
+	String C, L = "左", U = "上", D = "下", R = "右";
 	String Left, Up, Down, Right;
-	
+
 	URL HideimageURL = this.getClass().getResource("resources/84089164_480x480.png");
 	private ImageIcon Hide = new ImageIcon(HideimageURL); // 問題を隠している画像
 
@@ -63,114 +65,57 @@ public class TextPazzsample extends JFrame {
 		});
 	}
 
+
 	/**
 	 * Create the frame.
 	 */
 	public TextPazzsample() {
 		setTitle("Textvirsion");
+		Questions();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		URL Txturl = this.getClass().getResource("resources/one.txt");
-		// ファイル操作
-		try {
-			
-			File file = new File(Txturl.toURI());
-			FileReader filereader = new FileReader(file);
-			BufferedReader br = new BufferedReader(filereader);
-			String str = br.readLine();
-			Random rnd = new Random();
-			ArrayList<String> wordlist = new ArrayList<String>();//可変配列
-			
-			//仮
-			C ="中";
-			String work1,work2;
-			//テキストファイルの中身を全部配列に入れる
-			while(str != null) {
-				wordlist.add(str);
-				str = br.readLine();
-			}
-			br.close();	// ファイルを閉じる
-			ArrayList<String> firstlist = new ArrayList<String>();	//1文字目が解と同じ単語の配列
-			ArrayList<String> secondlist = new ArrayList<String>();	//2文字目が解と同じ単語の配列
-			for(String word: wordlist) {
-				work1 = word.substring(0,1);	// 1文字目を取り出す
-				work2 = word.substring(1,2);	// 2文字目を取り出す
-				if(C.equals(work1)) {		// 1文字目と解が同じなら
-					firstlist.add(word);	// 配列の要素を足す
-				}else if(C.equals(work2)) {	// 2文字目と解が同じなら
-					secondlist.add(word);	// 配列の要素を足す
-				}
-			}
-			
-			// 保存した配列からランダムに文字を取ってくる操作
-			Left = secondlist.get(rnd.nextInt(secondlist.size()));	// 配列の要素からランダムに取ってくる
-			while(true) {
-				Up = secondlist.get(rnd.nextInt(secondlist.size()));// 配列の要素からランダムに取ってくる
-				if(Left.equals(Up)) {								// 被ったらやり直し
-					continue;
-				}
-				break;	// 被ってないからループを抜ける
-			}
-			Down = firstlist.get(rnd.nextInt(firstlist.size()));		// 配列の要素からランダムに取ってくる
-			while(true) {
-				Right = firstlist.get(rnd.nextInt(firstlist.size()));	// 配列の要素からランダムに取ってくる
-				if(Down.equals(Right)) {								// 被ったらやり直し
-					continue;
-				}
-				break;	// 被ってないからループを抜ける
-			}
-			// ラベルに表示する変数に文字を入れる
-			L = Left.substring(0,1);
-			U = Up.substring(0,1);
-			D = Down.substring(1,2);
-			R = Right.substring(1,2);
-			
-		}catch(IOException | URISyntaxException e) {
-			System.out.println(e);
-		}
-		
-			
+
 		LeftLabel = new JLabel(L);
 		LeftLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		LeftLabel.setFont(new Font("MS 明朝", Font.PLAIN, 50));
 		LeftLabel.setBounds(10, 100, 80, 80);
 		contentPane.add(LeftLabel);
-		
+
 		UpLabel = new JLabel(U);
 		UpLabel.setFont(new Font("MS 明朝", Font.PLAIN, 50));
 		UpLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		UpLabel.setBounds(100, 10, 80, 80);
 		contentPane.add(UpLabel);
-		
-		CenterLabel = new JLabel("中");
+
+		CenterLabel = new JLabel(C);
 		CenterLabel.setForeground(Color.BLACK);
 		CenterLabel.setFont(new Font("MS 明朝", Font.PLAIN, 50));
 		CenterLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		CenterLabel.setBounds(100, 100, 80, 80);
 		contentPane.add(CenterLabel);
-		
+
 		DownLabel = new JLabel(D);
 		DownLabel.setFont(new Font("MS 明朝", Font.PLAIN, 50));
 		DownLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		DownLabel.setBounds(100, 190, 80, 80);
 		contentPane.add(DownLabel);
-		
+
 		RightLabel = new JLabel(R);
 		RightLabel.setFont(new Font("MS 明朝", Font.PLAIN, 50));
 		RightLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		RightLabel.setBounds(190, 100, 80, 80);
 		contentPane.add(RightLabel);
-		
-		/*黒い画像*/
+
+		/* 黒い画像 */
 		HideLabel = new JLabel(Hide);
 		HideLabel.setBounds(100, 100, 80, 80);
 		contentPane.add(HideLabel);
-		HideLabel.setVisible(true); //これで画像が見える（答えが見えなくなる）
-		
+		HideLabel.setVisible(true); // これで画像が見える（答えが見えなくなる）
+
 		JButton btnNewButton = new JButton("\u89E3\u7B54");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -178,51 +123,120 @@ public class TextPazzsample extends JFrame {
 				String ans = textField.getText();
 				if (ans.equals(C)) {
 					HideLabel.setVisible(false); // これで画像が見えなくなる（答えが見える）
-					String[] buttons = {"閉じる","次の問題へ","メニューへ戻る"};
-					int button = JOptionPane.showOptionDialog(null, "正解です", "判定結果",
-							JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE,null,buttons,buttons[0]);
-					if(button == 0) {
-						
-					}
-					else if(button == 1) {
-						
-					}
-					else if(button == 2) {
-						
+					String[] buttons = { "閉じる", "次の問題へ", "メニューへ戻る" };
+					int button = JOptionPane.showOptionDialog(null, "正解です", "判定結果", JOptionPane.YES_NO_OPTION,
+							JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);
+					if (button == 0) {
+
+					} else if (button == 1) {
+
+					} else if (button == 2) {
+
 					}
 				} else {
 					HideLabel.setVisible(false); // これで画像が見えなくなる（答えが見える）
-					String[] buttons = {"閉じる","メニューへ戻る"};
-					int button = JOptionPane.showOptionDialog(null, "不正解です ", "判定結果",
-							JOptionPane.YES_NO_OPTION,JOptionPane.ERROR_MESSAGE,null,buttons,buttons[0]);
-					if(button == 0) {
-						
-					}
-					else if(button == 1) {
-						
+					String[] buttons = { "閉じる", "メニューへ戻る" };
+					int button = JOptionPane.showOptionDialog(null, "不正解です ", "判定結果", JOptionPane.YES_NO_OPTION,
+							JOptionPane.ERROR_MESSAGE, null, buttons, buttons[0]);
+					if (button == 0) {
+
+					} else if (button == 1) {
+
 					}
 				}
-				HideLabel.setVisible(true); //これで画像が見える（答えが見えなくなる）
+				HideLabel.setVisible(true); // これで画像が見える（答えが見えなくなる）
 			}
 		});
 		btnNewButton.setBounds(317, 202, 89, 21);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("\u30D2\u30F3\u30C81");
 		btnNewButton_1.setBounds(317, 20, 89, 21);
 		contentPane.add(btnNewButton_1);
-		
+
 		textField = new JTextField();
 		textField.setBounds(317, 173, 96, 19);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnNewButton_2 = new JButton("\u30D2\u30F3\u30C82");
 		btnNewButton_2.setBounds(317, 51, 89, 21);
 		contentPane.add(btnNewButton_2);
-		
+
 		JLabel lblNewLabel = new JLabel("\u30D2\u30F3\u30C8\u306E\u5185\u5BB9");
 		lblNewLabel.setBounds(317, 82, 89, 70);
 		contentPane.add(lblNewLabel);
 	}
+	public void Questions() {
+
+		// ファイル操作
+		try {
+
+			File jfile = new File(difflist[diffculty].toURI()); // 出題用熟語ファイル
+			File ansfile = new File(allansTxturl.toURI()); // 出題用解答文字ファイル
+			FileReader jfilereader = new FileReader(jfile);
+			FileReader ansfilereader = new FileReader(ansfile);
+			BufferedReader jbr = new BufferedReader(jfilereader);
+			BufferedReader ansbr = new BufferedReader(ansfilereader);
+			Random rnd = new Random();
+			ArrayList<String> wordlist = new ArrayList<String>();// 可変配列
+			ArrayList<String> anslist = new ArrayList<String>();// 可変配列
+			String work1, work2;
+			// 答えが入ってるの中身を全部配列に入れる
+			String s = ansbr.readLine();
+			while (s != null) {
+				anslist.add(s);
+				s = ansbr.readLine();
+			}
+			ansbr.close();
+			// テキストファイルの中身を全部配列に入れる
+			String str = jbr.readLine();
+			while (str != null) {
+				wordlist.add(str);
+				str = jbr.readLine();
+			}
+			jbr.close(); // ファイルを閉じる
+
+			C = anslist.get(rnd.nextInt(anslist.size())); // 解をランダム抽選
+
+			ArrayList<String> firstlist = new ArrayList<String>(); // 1文字目が解と同じ単語の配列
+			ArrayList<String> secondlist = new ArrayList<String>(); // 2文字目が解と同じ単語の配列
+			for (String word : wordlist) {
+				work1 = word.substring(0, 1); // 1文字目を取り出す
+				work2 = word.substring(1, 2); // 2文字目を取り出す
+				if (C.equals(work1)) { // 1文字目と解が同じなら
+					firstlist.add(word); // 配列の要素を足す
+				} else if (C.equals(work2)) { // 2文字目と解が同じなら
+					secondlist.add(word); // 配列の要素を足す
+				}
+			}
+
+			// 保存した配列からランダムに文字を取ってくる操作
+			Left = secondlist.get(rnd.nextInt(secondlist.size())); // 配列の要素からランダムに取ってくる
+			while (true) {
+				Up = secondlist.get(rnd.nextInt(secondlist.size()));// 配列の要素からランダムに取ってくる
+				if (Left.equals(Up)) { // 被ったらやり直し
+					continue;
+				}
+				break; // 被ってないからループを抜ける
+			}
+			Down = firstlist.get(rnd.nextInt(firstlist.size())); // 配列の要素からランダムに取ってくる
+			while (true) {
+				Right = firstlist.get(rnd.nextInt(firstlist.size())); // 配列の要素からランダムに取ってくる
+				if (Down.equals(Right)) { // 被ったらやり直し
+					continue;
+				}
+				break; // 被ってないからループを抜ける
+			}
+			// ラベルに表示する変数に文字を入れる
+			L = Left.substring(0, 1);
+			U = Up.substring(0, 1);
+			D = Down.substring(1, 2);
+			R = Right.substring(1, 2);
+
+		} catch (IOException | URISyntaxException e) {
+			System.out.println(e);
+		}
+	}
+
 }
